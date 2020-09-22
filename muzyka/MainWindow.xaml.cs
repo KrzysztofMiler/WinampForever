8ff0e1d29425c34bc19c8ab4a9598e50d87f642b
Krzysztof Miler <krzy.miler@gmail.com>
2020-09-22 21:03:13 +0200

    kolejkowanie

--- muzyka/MainWindow.xaml.cs
+++ muzyka/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -28,11 +29,13 @@ namespace muzyka
 
         MediaPlayer mediaPlayer = new MediaPlayer();
         string filename;
-     
+
+        List<string> files = new List<string>();
+        List<string> paths= new List<string>();
 
         private void BT_click_Play(object sender, RoutedEventArgs e)
         {
-            mediaPlayer.Play();
+            mediaPlayer.Play();            
         }        
 
         private void BT_click_Pause(object sender, RoutedEventArgs e)
@@ -44,7 +47,7 @@ namespace muzyka
         {
             mediaPlayer.Stop();
         }
-
+        
         private void BT_Cilck_Open(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog//otwieram okno dialogowe z win do pobierania
@@ -52,12 +55,26 @@ namespace muzyka
                 Multiselect = false,
                 DefaultExt = ".mp3"
 
-            };
-            bool? dialogOK = fileDialog.ShowDialog();
+            };           
+
+            bool? dialogOK = fileDialog.ShowDialog();//czy odpali okno
             if (dialogOK == true)
             {
                 filename = fileDialog.FileName;
-                TBFileName.Text = fileDialog.SafeFileName;
+                TBFileName.Text = fileDialog.SafeFileName;//ten tekścik na wyszukaniu ustawia
+
+
+                files.Add(fileDialog.SafeFileName);
+                paths.Add(fileDialog.FileName);
+
+                listBoxSongs.Items.Clear();//aby nie dublowało itp
+
+                for (int i = 0; i < files.Count; i++)
+                {
+                    listBoxSongs.Items.Add(files[i]);
+
+                }
+
                 mediaPlayer.Open(new Uri(filename));
 
             }
